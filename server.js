a468c01fca229c3f1c7c7ab3cb7f7e7403df867d
Marcin Bieniek <marcingim@gmail.com>
2022-09-28 16:12:35 +0200

    Add views folder with basic files & create acces to main and about pages

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use((req, res, next) =>  {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`))
+    };
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.show('home.html')
+});
+
+app.get('/about', (req, res) => {
+    res.show('about.html')
+});
 
 app.listen(8000);
